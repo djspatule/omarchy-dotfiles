1dd5dc10d7fcefce8eebcb5ccf24bfd3c6e66dc0
Lionel ARNAUD <djspatule@wanadoo.fr>
2026-03-12 14:36:36 +0100

    trying new VFS mode in rclone$

--- scripts/rclone_mount.sh
+++ scripts/rclone_mount.sh
@@ -5,4 +5,7 @@
 # --vfs-cache-mode writes = These flags control the VFS file caching options. File caching is necessary to make the VFS layer appear compatible with a normal file system. It can be disabled at the cost of some compatibility. For example you'll need to enable VFS caching if you want to read and write simultaneously to a file. 
 # mount = the most reliable option to do bidirectionnal sync.
 
-rclone --vfs-cache-mode writes mount Google\ Drive\ Perso: ~/Google\ Drive\ Perso &
+# rclone --vfs-cache-mode writes mount Google\ Drive\ Perso: ~/Google\ Drive\ Perso &
+
+#trying new vfs "full" mode....
+rclone --vfs-cache-mode full mount Google\ Drive\ Perso: ~/Google\ Drive\ Perso &
